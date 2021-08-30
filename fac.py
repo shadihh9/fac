9154b91d978a121384a41a5ec629d842c40e70ca
adel <shadihh9@gmail.com>
2021-08-30 10:09:40 +0300

    Update fac.py

--- fac.py
+++ fac.py
@@ -2,7 +2,7 @@ import mmh3
 import requests
 import codecs
  
-response = requests.get('https://cybersecurity.wtf/favicon.ico')
+response = requests.get('https://www.facebook.com/favicon.ico')
 favicon = codecs.encode(response.content,"base64")
 hash = mmh3.hash(favicon)
 print(hash)
