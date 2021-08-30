b9694072ec3364ee9a5942c44604f1d3a0f20806
adel <shadihh9@gmail.com>
2021-08-30 09:57:17 +0300

    Add files via upload

---
+++ fac.py
@@ -0,0 +1,9 @@
+import mmh3
+import requests
+import codecs
+
+response = requests.get('https://www.facebook.com/favicon.ico')
+favicon = codecs.encode(response.content,"base64")
+hash = mmh3.hash(favicon)
+print hash
+
