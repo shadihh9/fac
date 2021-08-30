a55634ff7e961aba63e22471e890c2cb82451d6e
adel <shadihh9@gmail.com>
2021-08-30 10:04:47 +0300

    Update fac.py

--- fac.py
+++ fac.py
@@ -5,5 +5,5 @@ import codecs
 response = requests.get('https://www.facebook.com/favicon.ico')
 favicon = codecs.encode(response.content,"base64")
 hash = mmh3.hash(favicon)
-print hash
+print ("hash")
 
